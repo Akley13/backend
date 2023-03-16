a31bc45f78fcaae0f067e75ead60315974c477e7
Akley Alves <akleyalvescr@gmail.com>
2023-03-16 18:02:13 -0300

    upd

---
+++ src/main/java/br/com/developeracademy/backend/alunos/dto/Aluno.java
@@ -0,0 +1,13 @@
+package br.com.developeracademy.backend.alunos.dto;
+
+
+import java.io.Serializable;
+
+public class Aluno implements Serializable {
+    public Long id;
+    public String nome;
+    public String genero;
+    public  Integer idade;
+}
+
+
---
+++ src/main/java/br/com/developeracademy/backend/disciplinas/dto/Disciplinas.java
@@ -0,0 +1,10 @@
+package br.com.developeracademy.backend.disciplinas.dto;
+
+import java.io.Serializable;
+
+public class Disciplinas implements Serializable {
+
+    Long id;
+    String nome;
+    
+}
