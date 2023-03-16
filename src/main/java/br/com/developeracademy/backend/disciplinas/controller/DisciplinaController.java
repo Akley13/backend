a6bb0c9e8030f7125857d77966bc22abb4f5e637
Akley Alves <akleyalvescr@gmail.com>
2023-03-16 19:22:42 -0300

    Update DisciplinaController.java

--- src/main/java/br/com/developeracademy/backend/disciplinas/controller/DisciplinaController.java
+++ src/main/java/br/com/developeracademy/backend/disciplinas/controller/DisciplinaController.java
@@ -1,7 +1,17 @@
 package br.com.developeracademy.backend.disciplinas.controller;
 
+import br.com.developeracademy.backend.disciplinas.entity.Disciplinas;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/disciplinas")
 public class DisciplinaController {
+
+    public ResponseEntity<String> insert(@RequestBody Disciplinas disciplinas){
+        return ResponseEntity.ok().body("Ainda não implementado");
+    }
+
 }
