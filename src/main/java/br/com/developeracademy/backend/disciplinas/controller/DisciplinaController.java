e98e263501b440d25fd731f305799c3bdfab5ba6
Akley Alves <akleyalvescr@gmail.com>
2023-03-16 19:13:38 -0300

    Create DisciplinaController.java

---
+++ src/main/java/br/com/developeracademy/backend/disciplinas/controller/DisciplinaController.java
@@ -0,0 +1,7 @@
+package br.com.developeracademy.backend.disciplinas.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DisciplinaController {
+}
